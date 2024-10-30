a4a9f6a67b4a89ad08e157038036fe17dbcfb217
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-30 01:32:38 -0500

    java

--- src/secuenciales/frm09.java
+++ src/secuenciales/frm09.java
@@ -1,8 +1,83 @@
 package secuenciales;
 
-import java.util.Scanner;
+import java.awt.EventQueue;
 
-public class frm09 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class frm09 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtNumero, txtSuma;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frm09 frame = new frm09();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public frm09() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 350, 250);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblNumero = new JLabel("Número (4 cifras) :");
+        lblNumero.setBounds(50, 50, 120, 30);
+        getContentPane().add(lblNumero);
+
+        JLabel lblSuma = new JLabel("Suma de cifras :");
+        lblSuma.setBounds(50, 100, 120, 30);
+        getContentPane().add(lblSuma);
+
+        txtNumero = new JTextField();
+        txtNumero.setBounds(180, 50, 80, 30);
+        txtNumero.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtNumero);
+
+        txtSuma = new JTextField();
+        txtSuma.setBounds(180, 100, 80, 30);
+        txtSuma.setFocusable(false);
+        txtSuma.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtSuma);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(125, 150, 100, 30);
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
+    }
+
+    protected void btnCalcular_actionPerformed() {
+        try {
+            int numero = Integer.parseInt(txtNumero.getText());
+
+            if (numero < 1000 || numero > 9999) {
+                txtSuma.setText("Inválido");
+                return;
+            }
+
+            int suma = 0;
+            int tempNumero = numero;
+            while (tempNumero > 0) {
+                suma += tempNumero % 10;
+                tempNumero /= 10;
+            }
+
+            txtSuma.setText(String.valueOf(suma));
+        } catch (NumberFormatException e) {
+            txtSuma.setText("Error");
+        }
+    }
 }
-}
+
     
